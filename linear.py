c39b4261be3c38f8cdd23e21b7535280e8347217
niharikaharish0-create <niharikaharish0@gmail.com>
2026-05-05 14:04:33 +0400

    Create linear.py

---
+++ linear.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+df = pd.read_csv
+X =df.iloc[:,:-1].values #features
+Y =df.iloc[:,-1].values
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2, random= 42)
+model = LinearRegression()
+model.fit(X_train, Y_train)
+st.title("Exam score Prediction Model")
+st.write("Enter the no. of hours you have studied for the exam")
+hours =  st.number_input("Hours Studied", min_value=0.0, step = 0.1)
+if st.button("Predict score"):
+  predicted_score = model.predict([[hours]])[0]
+  st.success(f"Predicted score:{predicted_score: .2f}")
+  st.write("Sample Training DATA")
+  st.dataframe(df)
